9b1cd09d275adac906131d6ee6ed02eb4b860331
srpomeroy <sean.pomeroy@rightscale.com>
2017-10-25 00:33:38 -0400

    initial commit

--- tags/tag_checker_policy/tag_checker.cat.rb
+++ tags/tag_checker_policy/tag_checker.cat.rb
@@ -24,10 +24,11 @@
 
 # Required prolog
 name 'Tag Checker'
-rs_ca_ver 20160622
+rs_ca_ver 20161221
 short_description "![Tag](https://s3.amazonaws.com/rs-pft/cat-logos/tag.png)\n
 Check for a tag and report which instances are missing it."
 long_description "Uses RCL to check for a tag and report which instances are missing it."
+#import "sys_log"
 
 ##################
 # User inputs    #
@@ -40,7 +41,6 @@ parameter "param_tag_key" do
   min_length 3
   allowed_pattern '^([a-zA-Z0-9-_]+:[a-zA-Z0-9-_]+,*)+$'
 end
-
 
 parameter "param_email" do
   category "Contact"
@@ -57,9 +57,6 @@ parameter "param_run_once" do
   allowed_values "true","false"
 end
 
-
-
-
 ################################
 # Outputs returned to the user #
 ################################
@@ -68,7 +65,6 @@ output "output_bad_instances" do
   category "Output"
   description "Instances missing the specified tag(s)."
 end
-
 
 ####################
 # OPERATIONS       #
@@ -94,7 +90,9 @@ end
 ##########################
 # Go through and find improperly tagged instances
 define launch_tag_checker($param_tag_key,$param_email,$param_run_once) return $bad_instances do
-
+  #call sys_log.set_task_target(@@deployment)
+  #call sys_log.summary("Launch")
+  
   # add deployment tags for the parameters and then tell tag_checker to go
   rs_cm.tags.multi_add(resource_hrefs: [@@deployment.href], tags: [join(["tagchecker:tag_key=",$param_tag_key])])
   rs_cm.tags.multi_add(resource_hrefs: [@@deployment.href], tags: [join(["tagchecker:check_frequency=",$parameter_check_frequency])])
@@ -104,19 +102,16 @@ define launch_tag_checker($param_tag_key,$param_email,$param_run_once) return $b
 
   if $param_run_once == "true"
     $time = now() + 30
-  rs_ss.scheduled_actions.create(execution_id: @@execution.id,
-                                 action: "terminate", 
-                                 first_occurrence: $time)
-
-
+    rs_ss.scheduled_actions.create(
+      execution_id: @@execution.id,
+      action: "terminate", 
+      first_occurrence: $time
+    )
   end
-
-
 end
 
 # Do the actual work of looking at the tags and identifying bad instances.
 define tag_checker() return $bad_instances do
-
   # Get the stored parameters from the deployment tags
   $tag_key = ""
   $check_frequency = 5
@@ -134,59 +129,49 @@ define tag_checker() return $bad_instances do
     end
   end
 
-
-
-    @instances_operational = rs_cm.instances.get(filter: ["state==operational"])
-    @instances_provisioned = rs_cm.instances.get(filter: ["state==provisioned"])
-    @instances_running = rs_cm.instances.get(filter: ["state==running"])
-
-
+  @instances_operational = rs_cm.instances.get(filter: ["state==operational"])
+  @instances_provisioned = rs_cm.instances.get(filter: ["state==provisioned"])
+  @instances_running = rs_cm.instances.get(filter: ["state==running"])
   @instances = @instances_operational + @instances_provisioned + @instances_running
-
+  
   $instances_hrefs = to_object(@instances)["hrefs"]
-
-  # call logger(@@deployment, "All instance hrefs:", to_s($instances_hrefs))
   $$bad_instances_array=[]
 
   foreach $hrefs in $instances_hrefs do
-  $instances_tags = rs_cm.tags.by_resource(resource_hrefs: [$hrefs])
-  $tag_info_array = $instances_tags[0]
-  # Loop through the tag info array and find any entries which DO NOT reference the tag(s) in question.
-  $param_tag_keys_array = split($tag_key, ",")  # make the parameter list an array so I can search stuff
-
-          foreach $tag_info_hash in $tag_info_array do
-
-                  # Create an array of the tags' namespace:key parts
-                  $tag_entry_ns_key_array=[]
-                  foreach $tag_entry in $tag_info_hash["tags"] do
-                    $tag_entry_ns_key_array << split($tag_entry["name"],"=")[0]
-                  end
-
-          # See if the desired keys are in the found tags and if not take note of the improperly tagged instances
-          if logic_not(contains?($tag_entry_ns_key_array, $param_tag_keys_array))
-
-              foreach $resource in $tag_info_hash["links"] do
-                $$bad_instances_array << $resource["href"]
-              end
-
-          end
-        end
-
-
-        $$bad_instances = to_s($$bad_instances_array)
-  end
-      # Send an alert email if there is at least one improperly tagged instance
-      if logic_not(empty?($$bad_instances_array))
-        call send_tags_alert_email($tag_key,$param_email)
+    $instances_tags = rs_cm.tags.by_resource(resource_hrefs: [$hrefs])
+    $tag_info_array = $instances_tags[0]
+    # Loop through the tag info array and find any entries which DO NOT reference the tag(s) in question.
+    $param_tag_keys_array = split($tag_key, ",")  # make the parameter list an array so I can search stuff
+    foreach $tag_info_hash in $tag_info_array do
+      # Create an array of the tags' namespace:key parts
+      $tag_entry_ns_key_array=[]
+      foreach $tag_entry in $tag_info_hash["tags"] do
+        $tag_entry_ns_key_array << split($tag_entry["name"],"=")[0]
       end
-end
 
+      # See if the desired keys are in the found tags and if not take note of the improperly tagged instances
+      if logic_not(contains?($tag_entry_ns_key_array, $param_tag_keys_array))
+        foreach $resource in $tag_info_hash["links"] do
+          $$bad_instances_array << $resource["href"]
+        end
+      end
+    end
+    $$bad_instances = to_s($$bad_instances_array)
+  end
+  
+  #call sys_log.detail($$bad_instances_array)
+
+  # Send an alert email if there is at least one improperly tagged instance
+  if logic_not(empty?($$bad_instances_array))
+    call send_tags_alert_email($tag_key,$param_email)
+  end
+end
 
 # Returns the RightScale shard for the account the given CAT is launched in.
 define find_shard() return $shard_number do
-  call find_account_number() retrieve $account_number
-  $account = rs_cm.get(href: "/api/accounts/" + $account_number)
+  $account = rs_cm.get(href: "/api/accounts/" + $$account_number)
   $shard_number = last(split(select($account[0]["links"], {"rel":"cluster"})[0]["href"],"/"))
+  #call sys_log.detail("RS Account Shard: " + $shard_number)
 end
 
 define find_account_name() return $account_name do
@@ -194,16 +179,17 @@ define find_account_name() return $account_name do
   $acct_link = select($session_info[0]["links"], {rel: "account"})
   $acct_href = $acct_link[0]["href"]
   $account_name = rs_cm.get(href: $acct_href).name
+  #call sys_log.detail("RS Account Name: " + $account_name)
 end
 
 define get_tags_for_resource(@resource) return $tags do
-# Returns all tags for a specified resource. Assumes that only one resource
-# is passed in, and will return tags for only the first resource in the collection.
+  # Returns all tags for a specified resource. Assumes that only one resource
+  # is passed in, and will return tags for only the first resource in the collection.
 
-# @param @resource [ResourceCollection] a ResourceCollection containing only a
-#   single resource for which to return tags
+  # @param @resource [ResourceCollection] a ResourceCollection containing only a
+  #   single resource for which to return tags
 
-# @return $tags [Array<String>] an array of tags assigned to @resource
+  # @return $tags [Array<String>] an array of tags assigned to @resource
   $tags = []
   $tags_response = rs_cm.tags.by_resource(resource_hrefs: [@resource.href])
   $inner_tags_ary = first(first($tags_response))["tags"]
@@ -214,109 +200,129 @@ define get_tags_for_resource(@resource) return $tags do
 end
 
 define send_tags_alert_email($tags,$to) do
-
   # Get account ID
   call find_account_name() retrieve $account_name
+  call find_account_number() retrieve $$account_number
+  call find_shard() retrieve $$shard
 
   # Build email
-
-
-  $list_of_instances=""
-  $table_start="<td align=%22left%22 valign=%22top%22>"
-  $table_end="</td>"
-  $email_msg = "RightScale discovered that the following instance are missing tags <b>" + $tags + "</b> in <b>"+ $account_name +".</b> Per the policy set by your organization, these instances are not compliant"
-
-  $header="\<\!DOCTYPE html PUBLIC \"-\/\/W3C\/\/DTD XHTML 1.0 Transitional\/\/EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"\>
-  <html xmlns=\"http:\/\/www.w3.org\/1999\/xhtml\">
-      <head>
-          <meta http-equiv=%22Content-Type%22 content=%22text/html; charset=UTF-8%22 />
-          <a href=%22//www.rightscale.com%22>
-<img src=%22https://assets.rightscale.com/6d1cee0ec0ca7140cd8701ef7e7dceb18a91ba20/web/images/logo.png%22 alt=%22RightScale Logo%22 width=%22200px%22 />
-</a>
-          <style></style>
-      </head>
-      <body>
-        <table border=%220%22 cellpadding=%220%22 cellspacing=%220%22 height=%22100%%22 width=%22100%%22 id=%22bodyTable%22>
-            <tr>
-                <td align=%22left%22 valign=%22top%22>
-                    <table border=%220%22 cellpadding=%2220%22 cellspacing=%220%22 width=%22100%%22 id=%22emailContainer%22>
-                        <tr>
-                            <td align=%22left%22 valign=%22top%22>
-                                <table border=%220%22 cellpadding=%2220%22 cellspacing=%220%22 width=%22100%%22 id=%22emailHeader%22>
-                                    <tr>
-                                        <td align=%22left%22 valign=%22top%22>
-                                           " + $email_msg + "
-                                        </td>
-
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td align=%22left%22 valign=%22top%22>
-                                <table border=%220%22 cellpadding=%2210%22 cellspacing=%220%22 width=%22100%%22 id=%22emailBody%22>
-                                    <tr>
-                                        <td align=%22left%22 valign=%22top%22>
-                                            Instance Name
-                                        </td>
-                                        <td align=%22left%22 valign=%22top%22>
-                                            State
-                                        </td>
-                                        <td align=%22left%22 valign=%22top%22>
-                                            Link
-                                        </td>
-                                    </tr>
-                                    "
-
-
-
-    $footer="</tr></table></td></tr><tr><td align=%22left%22 valign=%22top%22><table border=%220%22 cellpadding=%2220%22 cellspacing=%220%22 width=%22100%%22 id=%22emailFooter%22><tr><td align=%22left%22 valign=%22top%22>
-            This report was automatically generated by a policy template tag checker your organization has defined in RightScale.
-        </td></tr></table></td></tr></table></td></tr></table></body></html>"
-
-
-
-
-
-
-
-
-  #$to = gsub($to,"@","%40")
   $subject = "Tag Checker Policy: "
   $from = "policy-cat@services.rightscale.com"
+  $email_msg = "RightScale discovered that the following instance are missing tags <b>" + $tags + "</b> in <b>"+ $account_name +".</b> Per the policy set by your organization, these instances are not compliant"
+  
+  $table_start="<td align=%22left%22 valign=%22top%22>"
+  $table_end="</td>"
+  $header="
+    \<\!DOCTYPE html PUBLIC \"-\/\/W3C\/\/DTD XHTML 1.0 Transitional\/\/EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"\>
+    <html xmlns=\"http:\/\/www.w3.org\/1999\/xhtml\">
+    <head>
+      <meta http-equiv=%22Content-Type%22 content=%22text/html; charset=UTF-8%22 />
+      <a href=%22//www.rightscale.com%22>
+      <img src=%22https://assets.rightscale.com/6d1cee0ec0ca7140cd8701ef7e7dceb18a91ba20/web/images/logo.png%22 alt=%22RightScale Logo%22 width=%22200px%22 /></a>
+    <style></style>
+    </head>
+    <body>
+      <table border=%220%22 cellpadding=%220%22 cellspacing=%220%22 height=%22100%%22 width=%22100%%22 id=%22bodyTable%22>
+        <tr>
+          <td align=%22left%22 valign=%22top%22>
+            <table border=%220%22 cellpadding=%2220%22 cellspacing=%220%22 width=%22100%%22 id=%22emailContainer%22>
+              <tr>
+                <td align=%22left%22 valign=%22top%22>
+                  <table border=%220%22 cellpadding=%2220%22 cellspacing=%220%22 width=%22100%%22 id=%22emailHeader%22>
+                    <tr>
+                      <td align=%22left%22 valign=%22top%22>
+                        " + $email_msg + "
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            <tr>
+              <td align=%22left%22 valign=%22top%22>
+                <table border=%220%22 cellpadding=%2210%22 cellspacing=%220%22 width=%22100%%22 id=%22emailBody%22>
+                  <tr>
+                    <td align=%22left%22 valign=%22top%22>
+                      Instance Name
+                    </td>
+                    <td align=%22left%22 valign=%22top%22>
+                      State
+                    </td>
+                    <td align=%22left%22 valign=%22top%22>
+                      Link
+                    </td>
+                  </tr>"
+  $footer="
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td align=%22left%22 valign=%22top%22>
+                  <table border=%220%22 cellpadding=%2220%22 cellspacing=%220%22 width=%22100%%22 id=%22emailFooter%22>
+                    <tr>
+                      <td align=%22left%22 valign=%22top%22>
+                        This report was automatically generated by a policy template tag checker your organization has defined in RightScale.
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      </table>
+    </body>
+  </html>"
 
-
-
+  $list_of_instances=""
   foreach $instance in $$bad_instances_array do
     $$instance_error = 'false'
     @server = rs_cm.servers.empty()
 
     sub on_error: set_instance_error() do
-    @server = rs_cm.get(href: $instance)
+      @server = rs_cm.get(href: $instance)
     end
 
     if $$instance_error == 'true'
       #issue with the instance, may no longer exists
-
     else
+      #call sys_log.detail("Instance Href: " + $instance)
+      
+      # Get instance name
+      if @server.state == 'provisioned'
+        $instance_name = @server.resource_uid
+      else
+        $instance_name = @server.name
+      end
+      if $instance_name == null
+        $instance_name = 'unknown'
+      end
+      #call sys_log.detail("Instance Name: " + $instance_name)
+      
+      # Get server state
+      if @server.state == null
+        $instance_state = 'unknown'
+      else
+        $instance_state = @server.state
+      end
+      #call sys_log.detail("Instance State: " + $instance_state)
 
-         if @server.state == 'provisioned'
-           $instance_name = @server.resource_uid
-         else
-           $instance_name = @server.name
-         end
-          $instance_state = @server.state
-          $server_access_link_root = 'unknown'
-
+      # Get server access link
+      $server_access_link_root = 'unknown'
       sub on_error: skip do
         call get_server_access_link(@server.href) retrieve $server_access_link_root
       end
-
+      if $server_access_link_root == null
+        $server_access_link_root = 'unknown'
+      end
+      #call sys_log.detail("Instance Link: " + $server_access_link_root)
+      
+      # Create the instance table
       $instance_table = "<tr>" + $table_start + $instance_name + $table_end + $table_start + $instance_state + $table_end + $table_start + $server_access_link_root + $table_end + "</tr>"
+      #call sys_log.detail("Instance Table Entry:" + $instance_table)
       insert($list_of_instances, -1, $instance_table)
     end
   end
-
 
   $email_body = $header + $list_of_instances + $footer
 
@@ -325,9 +331,7 @@ define send_tags_alert_email($tags,$to) do
   if $response['code'] != 200
     raise 'Failed to send email report: ' + to_s($response)
   end
-
 end
-
 
 define send_html_email($to, $from, $subject, $html) return $response do
   $mailgun_endpoint = 'http://smtp.services.rightscale.com/v3/services.rightscale.com/messages'
@@ -352,38 +356,36 @@ define set_instance_error() do
   $_error_behavior = "skip"
 end
 
-
 # Returns the RightScale account number in which the CAT was launched.
 define find_account_number() return $account_id do
   $session = rs_cm.sessions.index(view: "whoami")
   $account_id = last(split(select($session[0]["links"], {"rel":"account"})[0]["href"],"/"))
+  #call sys_log.detail("RS Account Number: " + $account_id)
 end
 
-
 define get_server_access_link($instance_href) return $server_access_link_root do
+  $rs_endpoint = "https://us-"+$$shard+".rightscale.com"
 
-    call find_shard() retrieve $shard
-    call find_account_number() retrieve $account_number
-    $rs_endpoint = "https://us-"+$shard+".rightscale.com"
-
-    $instance_id = last(split($instance_href, "/"))
-    $response = http_get(
-      url: $rs_endpoint+"/api/instances?ids="+$instance_id,
-      headers: {
+  $instance_id = last(split($instance_href, "/"))
+  $response = http_get(
+    url: $rs_endpoint+"/api/instances?ids="+$instance_id,
+    headers: {
       "X-Api-Version": "1.6",
-      "X-Account": $account_number
-      }
-     )
-     $instances = $response["body"]
-     $instance_of_interest = select($instances, { "href" : $instance_href })[0]
+      "X-Account": $$account_number
+    }
+  )
+  $instances = $response["body"]
+  $instance_of_interest = select($instances, { "href" : $instance_href })[0]
 
-     if $instance_of_interest["legacy_id"] == 'null'
-       $legacy_id = "unknown"
-     else
-       $legacy_id = $instance_of_interest["legacy_id"]
-     end
+  if $instance_of_interest["legacy_id"] == null
+    $legacy_id = "unknown"
+  else
+    $legacy_id = $instance_of_interest["legacy_id"]
+  end
 
-     $data = split($instance_href, "/")
-     $cloud_id = $data[3]
-     $server_access_link_root = "https://my.rightscale.com/acct/" + $account_number + "/clouds/" + $cloud_id + "/instances/" + $legacy_id
- end
+  #call sys_log.detail("Legacy ID: " + $legacy_id)
+
+  $data = split($instance_href, "/")
+  $cloud_id = $data[3]
+  $server_access_link_root = "https://my.rightscale.com/acct/" + $$account_number + "/clouds/" + $cloud_id + "/instances/" + $legacy_id
+end
