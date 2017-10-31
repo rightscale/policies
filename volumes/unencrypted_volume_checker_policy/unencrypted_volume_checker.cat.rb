527a517064abf192fad019bbcd8297864ac748fa
Richard Shade <rshade@rightscale.com>
2017-10-31 11:32:56 -0500

    updating code per peer review, shrinking list

--- volumes/unencrypted_volume_checker_policy/unencrypted_volume_checker.cat.rb
+++ volumes/unencrypted_volume_checker_policy/unencrypted_volume_checker.cat.rb
@@ -1,5 +1,5 @@
 name 'Encrypted Volume Checker Policy'
-rs_ca_ver 20160622
+rs_ca_ver 20161221
 short_description 'This automated policy CAT will find unencrypted EBS volumes and generate a report of them.'
 
 #Copyright 2017 RightScale
@@ -117,80 +117,5 @@ define send_email_mailgun($param_email) do
 end
 
 define cloud_lookup($href) return $cloud_name do
-  $clouds = {
-    "/api/clouds/1"    => "AWS US-East",
-    "/api/clouds/10"   => "AWS China-Beijing",
-    "/api/clouds/11"   => "AWS US-Ohio",
-    "/api/clouds/12"   => "AWS AP-Seoul",
-    "/api/clouds/13"   => "AWS EU-London",
-    "/api/clouds/14"   => "AWS CA-Central",
-    "/api/clouds/1415" => "CloudStack 2.2.13 - ESX",
-    "/api/clouds/1869" => "SoftLayer",
-    "/api/clouds/2"    => "AWS EU-Ireland",
-    "/api/clouds/2175" => "Google",
-    "/api/clouds/2178" => "Azure West US",
-    "/api/clouds/2179" => "Azure East US",
-    "/api/clouds/2180" => "Azure East Asia",
-    "/api/clouds/2181" => "Azure Southeast Asia",
-    "/api/clouds/2182" => "Azure North Europe",
-    "/api/clouds/2183" => "Azure West Europe",
-    "/api/clouds/2324" => "Rackspace Open Cloud - Chicago",
-    "/api/clouds/2373" => "Rackspace Open Cloud - London",
-    "/api/clouds/2374" => "Rackspace Open Cloud - Dallas",
-    "/api/clouds/2534" => "Rackspace Open Cloud - Sydney",
-    "/api/clouds/2535" => "BlueSkies",
-    "/api/clouds/2879" => "Rivervale-vMware",
-    "/api/clouds/3"    => "AWS US-West",
-    "/api/clouds/3032" => "RS-PS VM Test Env",
-    "/api/clouds/3037" => "vSphere Cloud",
-    "/api/clouds/3040" => "Azure Australia East",
-    "/api/clouds/3041" => "Azure Australia Southeast",
-    "/api/clouds/3070" => "Openstack Juno",
-    "/api/clouds/3243" => "Cisco OpenStack",
-    "/api/clouds/3301" => "Rivervale-CloudStack",
-    "/api/clouds/3302" => "Curts Big Ass Cloud",
-    "/api/clouds/3313" => "curts-cloud",
-    "/api/clouds/3338" => "Services UCA",
-    "/api/clouds/3339" => "tushar uca cloud",
-    "/api/clouds/3342" => "RSUCA",
-    "/api/clouds/3343" => "JerseyCloud",
-    "/api/clouds/3351" => "MobileStackZ",
-    "/api/clouds/3391" => "RCA-V VMware 5.5 Services",
-    "/api/clouds/3455" => "nathan-dockercloud",
-    "/api/clouds/3482" => "VMware Private Cloud",
-    "/api/clouds/3499" => "Openstack Liberty",
-    "/api/clouds/3509" => "Uncle Sean's Basement",
-    "/api/clouds/3518" => "AzureRM West US",
-    "/api/clouds/3519" => "AzureRM Japan East",
-    "/api/clouds/3520" => "AzureRM Southeast Asia",
-    "/api/clouds/3521" => "AzureRM Japan West",
-    "/api/clouds/3522" => "AzureRM East Asia",
-    "/api/clouds/3523" => "AzureRM East US",
-    "/api/clouds/3524" => "AzureRM West Europe",
-    "/api/clouds/3525" => "AzureRM North Central US",
-    "/api/clouds/3526" => "AzureRM Central US",
-    "/api/clouds/3527" => "AzureRM Canada Central",
-    "/api/clouds/3528" => "AzureRM North Europe",
-    "/api/clouds/3529" => "AzureRM Brazil South",
-    "/api/clouds/3530" => "AzureRM Canada East",
-    "/api/clouds/3531" => "AzureRM East US 2",
-    "/api/clouds/3532" => "AzureRM South Central US",
-    "/api/clouds/3546" => "AzureRM West US 2",
-    "/api/clouds/3547" => "AzureRM West Central US",
-    "/api/clouds/3567" => "AzureRM UK South",
-    "/api/clouds/3568" => "AzureRM UK West",
-    "/api/clouds/3569" => "AzureRM West India",
-    "/api/clouds/3570" => "AzureRM Central India",
-    "/api/clouds/3571" => "AzureRM South India",
-    "/api/clouds/3655" => "RCAV-Test",
-    "/api/clouds/3658" => "RS OSS RCAV",
-    "/api/clouds/4"    => "AWS AP-Singapore",
-    "/api/clouds/5"    => "AWS AP-Tokyo",
-    "/api/clouds/6"    => "AWS US-Oregon",
-    "/api/clouds/7"    => "AWS SA-São Paulo",
-    "/api/clouds/8"    => "AWS AP-Sydney",
-    "/api/clouds/9"    => "AWS EU-Frankfurt"
-  }
-
-  $cloud_name = $clouds[$href]
+  $cloud_name = rs_cm.get(href: $href).display_name
 end
