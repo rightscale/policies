b5386d04e38209cf4c65fd6bf8e21bfc7b5adc7a
Richard Shade <rshade@rightscale.com>
2018-01-19 08:37:34 -0600

    adding in sub and stop debugging

--- libraries/mailer.rb
+++ libraries/mailer.rb
@@ -22,64 +22,84 @@ import "sys_log"
 
 
 define delete_csv($endpoint,$filename) do
-  $endpoint = $endpoint + "/api/csv/" + $filename
-  $response = http_delete(
-    url: $endpoint,
-    headers: {"X-Api-Version": "1.0"}
-  )
+  call start_debugging()
+  sub on_error: stop_debugging() do
+    $endpoint = $endpoint + "/api/csv/" + $filename
+    $response = http_delete(
+      url: $endpoint,
+      headers: {"X-Api-Version": "1.0"}
+    )
+  end
 end
 
 define create_csv_with_columns($endpoint,$columns) return $filename do
   task_label("creating csv file")
+  $filename = ""
   $endpoint = $endpoint + "/api/csv/"
-  $response = http_post(
-    url: $endpoint,
-    headers: {"X-Api-Version": "1.0"},
-    body: { "data": [$columns] }
-  )
-  $filename = $response["body"]["file"]
-  $$mailer_filename = $filename
+  call sys_log.detail("endpoint" + $endpoint)
+  call start_debugging()
+  sub on_error: stop_debugging() do    
+    $response = http_post(
+      url: $endpoint,
+      headers: {"X-Api-Version": "1.0"},
+      body: { "data": [$columns] }
+    )
+    $filename = $response["body"]["file"]
+    $$mailer_filename = $filename
+  end
 end
 
 define update_csv_with_rows($endpoint,$filename,$rows) return $filename do
   task_label("csv:" + $filename + " name: " + $rows[0])
-  $endpoint = $endpoint + "/api/csv/" + $filename
-  $response = http_put(
-    url: $endpoint,
-    headers: {"X-Api-Version": "1.0"},
-    body: { "data": [$rows] }
-  )
-  $filename = $response["body"]["file"]
+  call start_debugging()
+  $filename = ""
+  sub on_error: stop_debugging() do
+    $endpoint = $endpoint + "/api/csv/" + $filename
+    $response = http_put(
+      url: $endpoint,
+      headers: {"X-Api-Version": "1.0"},
+      body: { "data": [$rows] }
+    )
+    $filename = $response["body"]["file"]
+  end
 end
 
 define send_html_email($endpoint,$to, $from, $subject, $html,$filename, $encoding) return $response do
   task_label("Sending email")
   $endpoint = $endpoint + "/api/mail"
-  $response = http_post(
-    url: $endpoint,
-    headers: {"X-Api-Version": "1.0"},
-    body: {
-    "to": $to,
-    "from": $from,
-    "subject": $subject,
-    "body": $html,
-    "attachment": $filename,
-    "encoding": $encoding
-    }
-  )
+  $response = ""
+  call start_debugging()
+  sub on_error: stop_debugging() do
+    $response = http_post(
+      url: $endpoint,
+      headers: {"X-Api-Version": "1.0"},
+      body: {
+      "to": $to,
+      "from": $from,
+      "subject": $subject,
+      "body": $html,
+      "attachment": $filename,
+      "encoding": $encoding
+      }
+    )
+  end
 end
 
-define create_csv_and_send_email($endpoint,$to, $from, $subject, $html,$two_dimensional_array_of_csv_data, $encoding) return $response do
+define create_csv_and_send_email($endpoint,$to,$from,$subject,$html,$two_dimensional_array_of_csv_data,$encoding) return $response do
   task_label("creating csv and send email")
   $endpoint = $endpoint + "/api/csv/"
-  $response = http_post(
-    url: $endpoint,
-    headers: {"X-Api-Version": "1.0"},
-    body: { "data": $two_dimensional_array_of_csv_data }
-  )
-  $filename = $response["body"]["file"]
-  $$mailer_filename = $filename
-  send_html_email($endpoint,$to, $from, $subject, $html,$filename, $encoding) retrieve $response
+  $response = ""
+  call start_debugging()
+  sub on_error:stop_debugging() do
+    $response = http_post(
+      url: $endpoint,
+      headers: {"X-Api-Version": "1.0"},
+      body: { "data": $two_dimensional_array_of_csv_data }
+    )
+    $filename = $response["body"]["file"]
+    $$mailer_filename = $filename
+  end
+  call send_html_email($endpoint,$to, $from, $subject, $html,$filename, $encoding) retrieve $response
 end
 
 define start_debugging() do
