c4f6684f973783cc84bcce9f3efadb4f5f7e1de7
egonzalez <edds718@gmail.com>
2018-02-05 17:09:40 -0500

    wip resizing policy

---
+++ instances/resize_instance_based_on_untilization.rb
@@ -0,0 +1,62 @@
+name "RightSize"	# <- Must be changed per OLC Version
+rs_ca_ver 20161221
+short_description "Downsize instances based on cpu and memory utilization"
+
+ 
+define find_rs_monitored_instances() do 
+
+    @all_instances = rs_cm.instances.index(filter:["state==operational"])
+      
+      
+      foreach @server in @all_instances do 
+        $tag = "rs_monitoring:state"
+          call has_tag(@server, $tag) retrieve $$tagged
+      end
+      
+     
+     
+      
+    end
+    
+    define has_tag(@server, $tag) return $$tagged do
+      call get_tags_for_resource(@server) retrieve $tags_on_server
+      
+      $href_tag = map $current_tag in $tags_on_server return $tag do
+        if $current_tag =~ $tag
+          $$tagged = "true"
+        end
+      end
+    end
+    
+    
+    define create_alert_spec() do
+        @@alert_specs = rs_cm.alertspecs.empty() 
+
+        @@hello =  rs_cm.alert_specs.create(
+            alert_spec:{
+                name:         "SSH SCALE UP",
+                description:  "Triggers autoscaling if users ssh into instances",
+                file:         "users/users",
+                variable:     "users", 
+                condition:    ">", 
+                threshold:    "0",
+                duration:     "1",
+                vote_type:    "grow",
+                vote_tag:     "node"
+            }
+        )
+      
+    end
+    
+    
+    
+    define get_tags_for_resource(@resource) return $tags do
+     
+      $tags = []
+      $tags_response = rs_cm.tags.by_resource(resource_hrefs: [@resource.href])
+      $inner_tags_ary = first(first($tags_response))["tags"]
+      $tags = map $current_tag in $inner_tags_ary return $tag do
+        $tag = $current_tag["name"]
+      end
+      $tags = $tags
+    end
