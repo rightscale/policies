6368fc554ea5b3a9483445e848b99b80266c9102
egonzalez <edds718@gmail.com>
2018-02-07 15:41:28 -0500

    testing global variables for metrics

--- instances/resize_instance_policy/resize_instance_based_on_untilization.rb
+++ instances/resize_instance_policy/resize_instance_based_on_untilization.rb
@@ -12,101 +12,99 @@ parameter "param_metric" do
     category "Metric"
     label "Utilization metric to monitor"
     type "string"
-    allowed_values "Idel CPU", "Free Memory"
-    #cpu-0/cpu-idle.value
-    #memory/memory-free.value 
-  end
+    allowed_values "Idle CPU | cpu-0/cpu-idle.value", "Free Memory | memory/memory-free.value"
+end
 
-  parameter "param_action" do
-    category "Configuration"
-    label "Policy Action"
-    type "string"
-    allowed_values "Report Only","Report and Resize"
-    default "Report Only"
-  end
+parameter "param_action" do
+  category "Configuration"
+  label "Policy Action"
+  type "string"
+  allowed_values "Report Only","Report and Resize"
+  default "Report Only"
+end
 
-  parameter "param_threshold" do
-    category "Configuration"
-    label "Metric Threshold Percentage"
-    type "number"
-  end
+parameter "param_threshold" do
+  category "Configuration"
+  label "Metric Threshold Percentage"
+  type "number"
+end
 
-  parameter "param_email" do
-    category "Contact"
-    label "Email addresses (separate with commas)"
-    type "string"
-  end
- 
+parameter "param_email" do
+  category "Contact"
+  label "Email addresses (separate with commas)"
+  type "string"
+end
+
 
 ##################
 # Operations     #
 ##################
 
 
-  operation "launch" do
-    description "Find long running instances"
-    definition "launch"
+operation "launch" do
+  description "Find long running instances"
+  definition "launch"
+end
+
+
+
+define launch($param_metric) return $param_metric do 
+
+$$metric = last(split($param_metric, "|"))
+$$metric_variable = first(split($$metric, "/"))
+
+@all_instances = rs_cm.instances.index(filter:["state==operational"])
+      
+    foreach @server in @all_instances do 
+      $tag = "rs_monitoring:edwin"  #edwin for testing, rs_monitoring:status should be the tag
+        call has_tag(@server,$tag) retrieve $tagged
+    end
+end
+
+#check for monitoring tag and call create alert spec 
+define has_tag(@server, $tag) return $tagged do
+  call get_tags_for_resource(@server) retrieve $tags_on_server
+  
+  $href_tag = map $current_tag in $tags_on_server return $tag do
+    if $current_tag =~ $tag
+      call create_alert_spec(@server) 
+      $tagged = "true"
+    end
   end
+end
 
-
-
-define launch() do 
-
-$metric_map = {
-    "CPU IDLE": "cpu-0/cpu-idle.value",
-    "MEMORY USAGE": "memory/memory-free.value"
-}
-
-    @all_instances = rs_cm.instances.index(filter:["state==operational"])
-      
-      
-      foreach @server in @all_instances do 
-        $tag = "rs_monitoring:edwin"  #edwin for testing, rs_monitoring:status should be the tag
-          call has_tag(@server, $tag) retrieve $tagged
-      end
-    end
     
-    define has_tag(@server, $tag) return $tagged do
-      call get_tags_for_resource(@server) retrieve $tags_on_server
-      
-      $href_tag = map $current_tag in $tags_on_server return $tag do
-        if $current_tag =~ $tag
-          call create_alert_spec(@server) 
-          $tagged = "true"
-        end
-      end
-    end
-    
-    
-    define create_alert_spec(@server) do
-        rs_cm.alert_specs.create(
-                alert_spec:{
-                    name:         "rightsizing_policy_" + @@execution.id,
-                    description:  "used by the resizing policy",
-                    file:         "users/users",
-                    variable:     "users", 
-                    condition:    ">", 
-                    threshold:    "0",
-                    duration:     "1", 
-                    vote_tag:      "rightsize",
-                    vote_type:     "shrink",
-                    subject_href:  @server.href
-                    
+#creates the alert spec 
+define create_alert_spec(@server)  do
+
+    rs_cm.alert_specs.create(
+            alert_spec:{
+                name:         "rightsizing_policy_" + @@execution.id,
+                description:  "used by the resizing policy",
+                file:         $$metric,
+                variable:     $$metric_variable, 
+                condition:    ">", 
+                threshold:    "0",
+                duration:     "1", 
+                vote_tag:      "rightsize",
+                vote_type:     "shrink",
+                subject_href:  @server.href
                 
-                }
-            )
-      
-    end
+            
+            }
+        )
+  
+end
+
     
     
-    
-    define get_tags_for_resource(@resource) return $tags do
-     
-      $tags = []
-      $tags_response = rs_cm.tags.by_resource(resource_hrefs: [@resource.href])
-      $inner_tags_ary = first(first($tags_response))["tags"]
-      $tags = map $current_tag in $inner_tags_ary return $tag do
-        $tag = $current_tag["name"]
-      end
-      $tags = $tags
-    end
+define get_tags_for_resource(@resource) return $tags do
+  
+  $tags = []
+  $tags_response = rs_cm.tags.by_resource(resource_hrefs: [@resource.href])
+  $inner_tags_ary = first(first($tags_response))["tags"]
+  $tags = map $current_tag in $inner_tags_ary return $tag do
+    $tag = $current_tag["name"]
+  end
+  $tags = $tags
+end
