8eba903d704d02b7ad3fcc24efbb4daafe81a2ef
Richard Shade <rshade@rightscale.com>
2018-04-17 15:00:59 -0500

    adding in average

--- policy_templates/instances/resize_instance_policy/resize-instance-based-on-utilization.pt.rb
+++ policy_templates/instances/resize_instance_policy/resize-instance-based-on-utilization.pt.rb
@@ -30,67 +30,78 @@ end
 
 auth "rs", type: "rightscale"
 
-resources "instances", type: "rs_cm.instances" do
-  cloud $cloud
-  tag $instance_tag
+resources "instances_us_east_1", type: "rs_cm.instances" do
+  cloud_href "/api/clouds/1"
 end
 
+resources "instances_us_west_2", type: "rs_cm.instances" do
+  cloud_href "/api/clouds/6"
+end
+
+datasource "instances_us_east_1" do
+    field "href",   val(@instances_us_east_1,'href')
+    field "id",     val(@instances_us_east_1,'resource_uid')
+    field "name",   val(@instances_us_east_1,'name')
+    field "state",  val(@instances_us_east_1,'state')
+    field "cloud",  val(@instances_us_east_1,'cloud')
+end
+
+datasource "instances_us_west_2" do
+    field "href",   val(@instances_us_west_2,'href')
+    field "id",     val(@instances_us_west_2,'resource_uid')
+    field "name",   val(@instances_us_west_2,'name')
+    field "state",  val(@instances_us_west_2,'state')
+    field "cloud",  val(@instances_us_west_2,'cloud')
+end
 # produces an array of objects [{ "average_mem"=>44, "id"=>"123bc", "type"=>"m1.small" }]
 
 datasource "instance_metrics" do
   request do
     auth $rs
     host rs_cm_host
-    path join([@instances.href, "monitoring_metrics", $monitoring_slug, "data"], "/")
+    path join([val(@instances_us_east_1,'href'), "monitoring_metrics", $monitoring_slug, "data"], "/")
     query "start", -300
     query "end", 0
     header "X-API-Version", "1.5"
   end
   result do
-    field "href",        @instances.href
+    field "href",        val(@instances_us_east_1,'href')
     field "average_mem", avg(jmes_path(data, "variables_data[].points[]"))
-    field "id",          @instances.resource_uid
-    field "type",        @instances.instance_type
+    field "id",          val(@instances_us_east_1,'resource_uid')
+    field "type",        val(@instances_us_east_1,'instance_type')
   end
 end
 
 policy "rightsize" do
   validate $instance_metrics do
+    template <<-EOS
+    Instances
+    The following instances are unattached:
+    { range data }
+    * Region: { $.cloud }
+    * Name: { $.name }
+    * State: { $.state }
+    * HREF: { $.href }
+    { end }
+    EOS
     escalate $downsize
-    check_over "2h", lt(data["average_mem"], $min_memory_percent)
+    #check_over "2h", lt(data["average_mem"], $min_memory_percent)
   end
   validate $instance_metrics do
+    template <<-EOS
+    Instances
+    The following instances are unattached:
+    { range data }
+    * Region: { $.cloud }
+    * Name: { $.name }
+    * State: { $.state }
+    * HREF: { $.href }
+    { end }
+    EOS
     escalate $upsize
-    check_over "2h", gt(data["average_mem"], $max_memory_percent)
+    #check_over "2h", gt(data["average_mem"], $max_memory_percent)
   end
 end
-
-# Do we need this?
-
-# data_source "instance_metrics" do
-#   auth $rs
-#   url concat(@instances.href, "/monitoring_metrics?title=memory")
-#   id    "href":        @instances.href
-#   field "average_mem": average(data)
-# end
-
-# data_source "reported_instances" do
-#   auth $rs
-#   url concat($instance_metrics.href, "/monitoring_metrics?title=memory")
-#   id    "href":        data.href
-#   field "average_mem": average(data)
-#   field "id":          $instance_metrics.resource_uid
-#   field "type":        data.instance_type
-# end
-
-# policy "rightsize" do
-#   validate $instances, escalate @downsize, $reported_instances do
-#     over "2h", lesser_than(data.average_mem, $min_memory_percent)
-#   end
-#   validate $instances, escalate @upsize, $reported_instances do
-#     over "2h", greater_than(data.average_mem, $max_memory_percent)
-#   end
-# end
 
 escalation "upsize" do
   email $escalate_to do
@@ -100,14 +111,14 @@ escalation "upsize" do
   {- end }
   EOS
   end
-  run "right_size_instance", true, data["href"]
+  run "right_size_instance", true, data
 end
 
 escalation "downsize" do
   email $escalate_to do
     subject_template "instance {data.id} of type {data.type} is using less than {$min_memory_percent}% memory and will be downgraded."
   end
-  run "right_size_instance", false, data["href"]
+  run "right_size_instance", false, data
 end
 
 # RCL definition that can be used in escalations
